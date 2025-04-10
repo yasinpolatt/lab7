88a98c9aa5ec9358efb4b6bd1d396d7a26db02f6
yasinpolatt <134144796+yasinpolatt@users.noreply.github.com>
2025-04-10 03:29:50 -0700

    Create string_analyzer.py

---
+++ string_analyzer.py
@@ -0,0 +1,9 @@
+from string_package import capitalize_words,reverse_string,count_words,count_characters,remove_punctuation,average_word_length
+
+
+print(capitalize_words("merhaba Dünya"))
+print(count_characters("merhaba Dünya"))
+print(reverse_string("merhaba Dünya"))
+print(count_words("merhaba Dünya"))
+print(remove_punctuation("merhaba Dünya"))
+print(average_word_length("merhaba Dünya"))
