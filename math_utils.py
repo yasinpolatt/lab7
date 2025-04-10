8cc82b7a571b1854832144c507b4aa0324bc0cd1
yasinpolatt <134144796+yasinpolatt@users.noreply.github.com>
2025-04-10 03:24:56 -0700

    Create math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    if(y == 0):
+        print("Y can not be 0")
+    return x/y
+def power(x,y):
+    return x**y
+def mod(x,y):
+    return x%y
